3e88bdd5e840c56374576ec1167810df31a36763
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-12-10 17:05:52 +0530

    day 10 part 1 done bitmask

---
+++ d10.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+lines = open("input.txt").read().splitlines()
+
+entities = [line.split() for line in lines]
+patterns, buttons, joltages = [], [], []
+
+for entity in entities:
+    pattern, button = entity[0][1:-1], entity[1:-1]
+    
+    n = len(pattern)
+    
+    main_bmask = 0
+    for c in pattern:
+        main_bmask = (main_bmask << 1) | (1 if c == '#' else 0)
+    patterns.append(main_bmask)
+    
+    button_bmasks = []
+    for b in button:
+        bmask = 0
+        for c in b[1:-1].split(','):
+            bmask |= (1 << (n - 1 - int(c)))
+        button_bmasks.append(bmask)
+    buttons.append(button_bmasks)
+    
+    joltages.append(eval(entity[-1]))
+
+# print([bin(p) for p in patterns])
+# print([[bin(b) for b in button_list] for button_list in buttons])
+# print(joltages)
+
+# ['0b110', '0b10', '0b11101']
+# [['0b1', '0b101', '0b10', '0b11', '0b1010', '0b1100'], ['0b10111', '0b110', '0b10001', '0b11100', '0b1111'], ['0b111110', '0b100110', '0b111011', '0b11000']]
+# [{3, 4, 5, 7}, {2, 12, 5, 7}, {10, 11, 5}]
+
+def bfs(button_masks: list, pattern: int) -> int:
+    q = deque([(btn_mask, 1) for btn_mask in button_masks])
+    while q:
+        bmask, depth = q.popleft()
+        if bmask == pattern:
+            return depth
+        for next_bmask in button_masks:
+            q.append((next_bmask ^ bmask, depth + 1))
+    return -1
+
+total = 0
+for i in range(len(entities)):
+    steps = bfs(buttons[i], patterns[i])
+    total += steps
+    
+print(total)
