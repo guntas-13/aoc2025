9aef31801954c8a75140cd19368e5f5933d1a9d7
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-12-11 13:55:08 +0530

    day 11 p2 done nice logic

--- d11.py
+++ d11.py
@@ -9,7 +9,7 @@ for line in lines:
         all_nodes.add(n)
     adj[nodes[0][:-1]] = nodes[1:]
 
-def dfs_p1(node: str, dest: str, adj: dict, dp: dict) -> int:
+def dfs(node: str, dest: str, adj: dict, dp: dict) -> int:
     if (node == dest):
         return 1
     
@@ -17,16 +17,36 @@ def dfs_p1(node: str, dest: str, adj: dict, dp: dict) -> int:
         return dp[node]
     
     res = 0
-    for nb in adj[node]:
-        res += dfs_p1(nb, dest, adj, dp)
+    if node in adj:
+        for nb in adj[node]:
+            res += dfs(nb, dest, adj, dp)
     
     dp[node] = res
     return res
 
 n = len(all_nodes)
-dp = {x: -1 for x in all_nodes}
-print(dfs_p1('you', 'out', adj, dp))
+print(dfs('you', 'out', adj, {x: -1 for x in all_nodes}))
 # 571
 
-# print(dfs_p1('svr', 'out', adj, dp))
+print(dfs('svr', 'out', adj, {x: -1 for x in all_nodes}))
 # 683903757252542732
+
+# p2
+s2f = dfs('svr', 'fft', adj, {x: -1 for x in all_nodes})
+f2d = dfs('fft', 'dac', adj, {x: -1 for x in all_nodes})
+d2o = dfs('dac', 'out', adj, {x: -1 for x in all_nodes})
+
+s2d = dfs('svr', 'dac', adj, {x: -1 for x in all_nodes})
+d2f = dfs('dac', 'fft', adj, {x: -1 for x in all_nodes})
+f2o = dfs('fft', 'out', adj, {x: -1 for x in all_nodes})
+
+print(s2d, d2f, f2o)
+# 770085400930 0 3879174736863
+print(s2f, f2d, d2o)
+# 24087 4368408 4860
+
+# svr -> fft -> dac -> out
+p1 = s2f * f2d * d2o
+# svr -> dac -> fft -> out
+p2 = s2d * d2f * f2o
+print(p1 + p2)
